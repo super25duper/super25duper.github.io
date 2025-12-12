942d13875bfc4e7928a7d08c01b85a30ce02a7ec
super25duper <yehonami2500@gmail.com>
2025-12-12 04:00:11 +0200

    Update firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpI1gSK5k48Nw2wldzsssMAwtrY6xc3Qc",
+  authDomain: "big-one-92549.firebaseapp.com",
+  databaseURL: "https://big-one-92549-default-rtdb.firebaseio.com",
+  projectId: "big-one-92549",
+  storageBucket: "big-one-92549.firebasestorage.app",
+  messagingSenderId: "996052489124",
+  appId: "1:996052489124:web:1e41982de561078a37a731",
+  measurementId: "G-BYLNLFTM28"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+
+
