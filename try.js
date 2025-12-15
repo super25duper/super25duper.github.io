b1ccb4d21398394498f8b61f6be6476889a19f6d
super25duper <yehonami2500@gmail.com>
2025-12-16 01:38:18 +0200

    Create try.js

---
+++ try.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBf-12UCoHpIMg81MWaer7DmtfVla95X9M",
+    authDomain: "try-a-69b75.firebaseapp.com",
+    projectId: "try-a-69b75",
+    storageBucket: "try-a-69b75.firebasestorage.app",
+    messagingSenderId: "136796358688",
+    appId: "1:136796358688:web:f6d41df5505e26ec0b5445",
+    measurementId: "G-ST0TTENTGF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
