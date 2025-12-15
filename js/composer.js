7a11e4cffbea44de710dbf14126cb61efe72c525
super25duper <yehonami2500@gmail.com>
2025-12-15 04:55:25 +0200

    Update composer.js

--- js/composer.js
+++ js/composer.js
@@ -7,24 +7,35 @@ const openMainBtn = document.getElementById("openMain");
 const mainComposer = document.getElementById("mainComposer");
 const mainText = document.getElementById("mainText");
 
+let composerOpen = false;
+
 /* פתיחת קומפוזר */
 openMainBtn.onclick = () => {
+  if (composerOpen) return;
+  composerOpen = true;
+
   mainComposer.classList.remove("hidden");
   openMainBtn.style.display = "none";
 
+  // נותנים למקלדת זמן להיפתח
   setTimeout(() => {
     mainText.focus();
 
-    /* גלילה עדינה מעל המקלדת */
-    mainComposer.scrollIntoView({
-      behavior: "smooth",
-      block: "center"
-    });
-  }, 250);
+    // גלילה בטוחה – מעל המקלדת
+    setTimeout(() => {
+      mainComposer.scrollIntoView({
+        behavior: "smooth",
+        block: "end"
+      });
+    }, 200);
+
+  }, 150);
 };
 
 /* סגירת קומפוזר */
 window.closeMain = function () {
+  composerOpen = false;
+
   mainComposer.classList.add("hidden");
   openMainBtn.style.display = "block";
   mainText.value = "";
