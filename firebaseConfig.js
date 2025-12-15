d4dd7d3be297305ffb335b00491827bf5dcd2da2
super25duper <yehonami2500@gmail.com>
2025-12-15 19:24:04 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCpI1gSK5k48Nw2wldzsssMAwtrY6xc3Qc",
-  authDomain: "big-one-92549.firebaseapp.com",
-  databaseURL: "https://big-one-92549-default-rtdb.firebaseio.com",
-  projectId: "big-one-92549",
-  storageBucket: "big-one-92549.firebasestorage.app",
-  messagingSenderId: "996052489124",
-  appId: "1:996052489124:web:1e41982de561078a37a731",
-  measurementId: "G-BYLNLFTM28"
+  apiKey: "AIzaSyAb6crKn7LDVhYtFMUtErtZNKEL6lfW5pA",
+  authDomain: "bigone-86490.firebaseapp.com",
+  databaseURL: "https://bigone-86490-default-rtdb.firebaseio.com",
+  projectId: "bigone-86490",
+  storageBucket: "bigone-86490.firebasestorage.app",
+  messagingSenderId: "543555505186",
+  appId: "1:543555505186:web:46f992f414561830b50e7b",
+  measurementId: "G-GVPSS1D63D"
 };
 
 firebase.initializeApp(firebaseConfig);
