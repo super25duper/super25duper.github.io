4970b8067e742f980c1d7e55dccbf4f03f53d325
super25duper <yehonami2500@gmail.com>
2025-12-16 00:56:59 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   authDomain: "bigone-86490.firebaseapp.com",
   databaseURL: "https://bigone-86490-default-rtdb.firebaseio.com",
   projectId: "bigone-86490",
-  storageBucket: "bigone-86490.appspot.com",
+  storageBucket: "bigone-86490.firebasestorage.app",
   messagingSenderId: "543555505186",
   appId: "1:543555505186:web:46f992f414561830b50e7b",
   measurementId: "G-GVPSS1D63D"
