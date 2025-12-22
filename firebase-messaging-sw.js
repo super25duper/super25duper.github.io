ea9abae922af1a7853d6a16df9f9320825bb89a8
super25duper <yehonami2500@gmail.com>
2025-12-22 15:28:24 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAb6crKn7LDVhYtFMUtErtZNKEL6lfW5pA",
+  authDomain: "bigone-86490.firebaseapp.com",
+  projectId: "bigone-86490",
+  messagingSenderId: "543555505186",
+  appId: "1:543555505186:web:46f992f414561830b50e7b"
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  return self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body
+    }
+  );
+});
