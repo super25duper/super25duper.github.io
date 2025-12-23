cb44a097a5c4c61a74cb2d560abe8c8b4f87be11
super25duper <yehonami2500@gmail.com>
2025-12-23 22:42:51 +0200

    Update firebaseConfig B.js

--- firebaseConfig B.js
+++ firebaseConfig B.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAb6crKn7LDVhYtFMUtErtZNKEL6lfW5pA",
+  apiKey: "AIzaSyDN_LjOucEM9Wx9JgHL2PgTVGGjJmEgdAY",
   authDomain: "bigone-86490.firebaseapp.com",
   databaseURL: "https://bigone-86490-default-rtdb.firebaseio.com",
   projectId: "bigone-86490",
   storageBucket: "bigone-86490.firebasestorage.app",
   messagingSenderId: "543555505186",
-  appId: "1:543555505186:web:46f992f414561830b50e7b",
-  measurementId: "G-GVPSS1D63D"
+  appId: "1:543555505186:web:6474dab4c51dc49db50e7b",
+  measurementId: "G-7PG3MCK0SW"
 };
 
 firebase.initializeApp(firebaseConfig);
