1eaddb236381c61676656e1668f1ab394bd3e864
super25duper <yehonami2500@gmail.com>
2025-12-23 22:37:28 +0200

    Add files via upload

---
+++ firebaseConfig B.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAb6crKn7LDVhYtFMUtErtZNKEL6lfW5pA",
+  authDomain: "bigone-86490.firebaseapp.com",
+  databaseURL: "https://bigone-86490-default-rtdb.firebaseio.com",
+  projectId: "bigone-86490",
+  storageBucket: "bigone-86490.firebasestorage.app",
+  messagingSenderId: "543555505186",
+  appId: "1:543555505186:web:46f992f414561830b50e7b",
+  measurementId: "G-GVPSS1D63D"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+
+
